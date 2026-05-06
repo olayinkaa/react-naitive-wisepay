dfe860c83d183c4ffaf184c08eef5bbaa26c219f
olayinka.ibrahim <olayinka.ibrahim@etranzact.com>
2026-05-06 08:52:41 +0100

    code updated

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,10 +1,42 @@
+import { colors } from "@/constants/theme";
 import { Ionicons, MaterialCommunityIcons } from "@expo/vector-icons";
+import { BlurView } from "expo-blur";
 import { Tabs } from "expo-router";
 import React from "react";
+import { StyleSheet } from "react-native";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
 
-export default function _layout() {
+export default function TabLayout() {
+  const inset = useSafeAreaInsets();
   return (
-    <Tabs>
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: colors.primary,
+        tabBarStyle: {
+          position: "absolute",
+          backgroundColor: "transparent",
+          height: 32 + inset.bottom,
+          paddingTop: 0,
+          borderWidth: 1,
+          elevation: 0,
+          marginHorizontal: 20,
+          marginBottom: inset.bottom,
+          borderRadius: 24,
+          overflow: "hidden",
+        },
+        tabBarLabelStyle: {
+          fontSize: 12,
+          fontWeight: "600",
+        },
+        tabBarBackground: () => (
+          <BlurView
+            tint="light"
+            intensity={100}
+            style={StyleSheet.absoluteFill}
+          />
+        ),
+      }}
+    >
       <Tabs.Screen
         name="home"
         options={{
