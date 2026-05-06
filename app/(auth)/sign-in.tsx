5a764401c8746d95c8de3d468c230ba449611c4a
Ibrahim Olayinka James <ibrahimolayinkaa@gmail.com>
2026-05-06 19:18:20 +0100

    code updated

--- app/(auth)/sign-in.tsx
+++ app/(auth)/sign-in.tsx
@@ -65,7 +65,7 @@ export default function SignIn() {
         />
       </View>
 
-      <Link href="/home" className="self-end">
+      <Link href="/reset" className="self-end">
         <Text className="text-blue-600 font-medium">Forgot Password?</Text>
       </Link>
     </View>
